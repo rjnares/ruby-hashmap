8b43e8f8d489d00fc2ced7d4118da6adcdb0d92f
Roberto Nares <roberto.nares@gmail.com>
2024-08-03 11:47:24 -0700

    Implement functionality to create hash code from key

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -10,6 +10,15 @@ class HashMap
     @load_factor = 0.75
   end
 
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+
+    hash_code
+  end
+
   private
 
   attr_reader :load_factor
--- main.rb
+++ main.rb
@@ -2,6 +2,23 @@
 
 require_relative 'lib/hash_map'
 
-test = HashMap.new
+hash_map = HashMap.new
 
-puts "Expected Length: 0, Actual Length: #{test.length}"
+puts "Expected Length: 0, Actual Length: #{hash_map.length}"
+
+test_key_set = [
+  'apple',
+  'banana',
+  'carrot',
+  'dog',
+  'elephant',
+  'frog',
+  'grape',
+  'hat',
+  'ice cream',
+  'jacket',
+  'kite',
+  'lion'
+]
+
+test_key_set.each { |key| puts "'#{key}' hash code: #{hash_map.hash(key)}" }
